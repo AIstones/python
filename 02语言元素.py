05f724e8cd526a388fcedcb119a559d7b4059252
Alex <707279282@qq.com>
2019-07-18 21:49:26 +0800

    语言元素

---
+++ 02语言元素.py
@@ -0,0 +1,27 @@
+# a = int(input('请输入321\n'))
+# b = int(input('请输入123\n'))
+# print(a + b)
+# print(a - b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+"""
+使用type()检查变量的类型
+"""
+
+# a = 100
+# b = 12.345
+# c = 1 + 5j
+# d = 'hello, world'
+# e = True
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+a = 48
+print(chr(a))
+print(ord('s'))
