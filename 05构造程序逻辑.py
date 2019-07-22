c8b9418d8e5646b76f6766164eec8cc5d2485f27
Alex <707279282@qq.com>
2019-07-22 10:06:20 +0800

    05构造程序逻辑

--- 05构造程序逻辑.py
+++ 05构造程序逻辑.py
@@ -28,3 +28,47 @@
 #     wanquanshu(n)
 
 
+"""
+百钱百鸡
+"""
+# for i in range(20):
+#     for j in range(34):
+#         if 15*i + 9*j + (100-i-j) == 300:
+#             print('公鸡有%d只， 母鸡有%d只， 雏鸡有%d' % (i, j, (100-i-j)))
+
+# a1 = 1
+# a2 = 1
+# li = [1, 1]
+# for n in range(3, 20):
+#     a3 = a1 + a2
+#     li.append(a3)
+#     a1, a2 = a2, a3
+
+# print(list(li))
+
+"""
+cpaps赌博游戏 
+
+说明：
+一个简单的赌博游戏，游戏规则如下：玩家掷两个骰子，点数为1到6，如果第一次点数和为7或11，则玩家胜，如果点数和为2、3
+或12，则玩家输，如果和 为其它点数，则记录第一次的点数和，然后继续掷骰，直至点数和等于第一次掷出的点数和，则玩家胜，
+如果在这之前掷出了点数和为7，则玩家输。
+
+解法:
+规则看来有些复杂，但是其实只要使用switch配合if条件判断来撰写即可，小心不要弄错胜负顺序即可。
+--------------------- 
+"""
+
+from random import randint
+
+
+while True:
+    i = randint(1, 7)
+    j = randint(1, 7)
+    if i + j == 7 or i + j ==11:
+        print('玩家胜')
+        break
+    elif i + j == 2 or i + j == 3 or i + j == 12:
+        print('玩家输')
+        break
+    
