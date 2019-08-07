84c10b72e72c20b613a61283dcdc30c96889cc97
Alex <707279282@qq.com>
2019-08-07 10:14:28 +0800

    9

--- 08面向对象编程基础.py
+++ 08面向对象编程基础.py
@@ -64,34 +64,34 @@
 #         sleep(1)
 #         clock.run()
 
-from math import sqrt
+# from math import sqrt
 
-class Point(object):
-    def __init__(self, x=0, y=0):
+# class Point(object):
+#     def __init__(self, x=0, y=0):
 
-        self.x = x
-        self.y = y
-    def move_to(self, x, y):
-        self.x += x
-        self.y += y
-    def move_by(self, dx, dy):
-        self.x += dx
-        self.y += dy
-    def distance_to(self, other):
-        dx = self.x - other.x
-        dy = self.y - other.y
-        return sqrt(dx ** 2 + dy ** 2)
-    def __str__(self):
-        return '(%s, %s)' % (str(self.x), str(self.y))
+#         self.x = x
+#         self.y = y
+#     def move_to(self, x, y):
+#         self.x += x
+#         self.y += y
+#     def move_by(self, dx, dy):
+#         self.x += dx
+#         self.y += dy
+#     def distance_to(self, other):
+#         dx = self.x - other.x
+#         dy = self.y - other.y
+#         return sqrt(dx ** 2 + dy ** 2)
+#     def __str__(self):
+#         return '(%s, %s)' % (str(self.x), str(self.y))
 
-def main():
-    p1 = Point(3, 5)
-    p2 = Point()
-    print(p1)
-    print(p2)
-    p2.move_by(-1, 2)
-    print(p2)
-    print(p1.distance_to(p2))
+# def main():
+#     p1 = Point(3, 5)
+#     p2 = Point()
+#     print(p1)
+#     print(p2)
+#     p2.move_by(-1, 2)
+#     print(p2)
+#     print(p1.distance_to(p2))
 
         
 
--- 09面对对象进阶.py
+++ 09面对对象进阶.py
@@ -1,31 +1,48 @@
-class Person(object):
-    def __init__(self, name, age):
-        self._name = name
-        self._age = age
+# class Person(object):
+#     def __init__(self, name, age):
+#         self._name = name
+#         self._age = age
 
-    # 访问器 - getter方法
-    @property
-    def name(self):
-        return self._name
+#     # 访问器 - getter方法
+#     @property
+#     def name(self):
+#         return self._name
 
-    @property
-    def age(self):
-        return self._age
+#     @property
+#     def age(self):
+#         return self._age
     
-    @age.setter
-    def age(self, age):
-        self._age = age
-    def play(self):
-        if self._age <= 16:
-            print('%s正在玩飞行棋。' % self._name)
-        else:
-            print('%s正在玩斗地主。' % self._name)
+#     @age.setter
+#     def age(self, age):
+#         self._age = age
+#     def play(self):
+#         if self._age <= 16:
+#             print('%s正在玩飞行棋。' % self._name)
+#         else:
+#             print('%s正在玩斗地主。' % self._name)
 
-def main():
-    person = Person('王大锤', 12)
-    person.play()
-    person.age = 22
-    person.play()
+# def main():
+#     person = Person('王大锤', 12)
+#     person.play()
+#     person.age = 22
+#     person.play()
+
+from math import sqrt
+
+class Triangle(object):
+    def __init__(self, a, b, c):
+        self._a = a 
+        self._b = b
+        self._c = c
+
+    @staticmethod
+    def is_valid(a, b, c):
+        return a + b > c and b + c > a and a + c > b
+    def perimeter(self):
+        return self._a + self._b + self._c
+    def area(self):
+        half = self.perimeter() / 2
+        return sqrt(half * (half - self._a) * )
 
 if __name__ == '__main__':
     main()
